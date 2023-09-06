b2ada2fec4e82f6716d83bdf5b808a07194afa87
Franol Fekadu <firatheking@gmail.com>
2023-09-06 12:19:08 +0300

    adding

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -43,12 +43,7 @@ class FirebaseCloudStorage{
       isEqualTo: ownerUserId,
     )
     .get()
-    .then((value) => value.docs.map((doc) {
-      return CloudNote(
-        documentId: doc.id, 
-        ownerUserId: doc.data()[ownerUserIdFieldName] as String, 
-        text: doc.data()[textFieldName] as String);
-          }
+    .then((value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc)
         )
       );
     } catch(e){
@@ -56,11 +51,13 @@ class FirebaseCloudStorage{
     }
   }
 
-  void createNewNote({required String ownerUserId}) async{
-    await notes.add({
+  Future<CloudNote> createNewNote({required String ownerUserId}) async{
+    final doc = await notes.add({
       ownerUserIdFieldName: ownerUserId,
       textFieldName: '' 
     });
+    final fetchedNote = await doc.get();
+    return CloudNote(documentId: fetchedNote.id, ownerUserId: ownerUserId, text: '');
   }
   
   static final FirebaseCloudStorage _shared = FirebaseCloudStorage._sharedInstance();
